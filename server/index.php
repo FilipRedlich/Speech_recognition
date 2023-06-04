795060979bc3c4b1742ff51cafa774b9e773e995
FilipRedlich <filipredlich.pr@gmail.com>
2023-06-04 13:15:39 +0200

    added table for displaying saved text

--- server/index.php
+++ server/index.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Speech Recog db</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -29,11 +30,27 @@
 
     $result = $con->query("SELECT recognized_text, timestamp from recognized_text");
 
-    while($row = $result->fetch_assoc())  {
-        echo $row["recognized_text"] . '<br>';
-    } 
-
     ?>
+
+    <div class="main-table">
+        <table class="table table-responsive">
+        <thead>
+            <tr class="table-success">
+                <td>Rozpoznany text</td>
+                <td>Timestamp</td>
+            </tr>
+            <?php while($row = $result->fetch_assoc())  { ?>
+            <tr class="table-data">
+                <td>
+                    <?=$row['recognized_text']?>
+                </td>
+                <td>
+                    <?=$row['timestamp']?>
+                </td>
+            </tr>
+            <?php } ?>
+        </thead>
+        </table>
 
 </body>
 
